96776ec00b105b0e40b3c54827bc350599d54f78
DW <daiwei2333@gmail.com>
2021-11-14 10:51:46 +0800

    day7

---
+++ 21.合并两个有序链表.java
@@ -0,0 +1,51 @@
+import java.util.List;
+
+/*
+ * @lc app=leetcode.cn id=21 lang=java
+ *
+ * [21] 合并两个有序链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+        ListNode pre = new ListNode(-1), cur = pre;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                cur.next = l1;
+                l1 = l1.next;
+            } else {
+                cur.next = l2;
+                l2 = l2.next;
+            }
+            cur = cur.next;
+        }
+        cur.next = l1 == null ? l2 : l1;
+        return pre.next;
+
+        // if (l1 == null) return l2;
+        // if (l2 == null) return l1;
+        // if (l1.val < l2.val) {
+        //     l1.next = mergeTwoLists(l1.next, l2);
+        //     return l1;
+        // } else {
+        //     l2.next = mergeTwoLists(l1, l2.next);
+        //     return l2;
+        // }
+    }
+
+}
+// @lc code=end
+
