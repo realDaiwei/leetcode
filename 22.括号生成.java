e326bf5252acb2cd4025d25033c07e8a3ee0c962
DW <daiwei2333@gmail.com>
2021-12-07 23:25:58 +0800

    day30

--- 22.括号生成.java
+++ 22.括号生成.java
@@ -11,20 +11,26 @@ import java.util.List;
 // @lc code=start
 class Solution {
     public List<String> generateParenthesis(int n) {
-        List<String> res = new ArrayList<>();
-        if (n == 0) return res;
-        generateParenthesis(n, 0, 0, "", res);
-        return res;
+        List<String> ans = new ArrayList<>();
+       if (n == 0) return ans;
+       slove(n, 0, 0, "", ans);
+       return ans;
     }
 
-    private void generateParenthesis(int n, int open, int close, String str, List<String> res) {
-        if (close == n)  {
-            res.add(str);
+    private void slove(int n, int open, int close, String str, List<String> ans) {
+        if (close == n) {
+            ans.add(str);
             return;
-        } 
-        if (open < n) generateParenthesis(n, open + 1, close, str + "(", res); 
-        if (close < open) generateParenthesis(n, open, close + 1, str + ")", res); 
+        }
+        if (open < n) {
+            slove(n, open + 1, close, str + "(", ans);
+        }
+        if (open > close) {
+            slove(n, open, close + 1, str + ")", ans);
+        }
     }
+
+    
 }
 // @lc code=end
 
---
+++ 515.在每个树行中找最大值.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.swing.tree.TreeNode;
+
+/*
+ * @lc app=leetcode.cn id=515 lang=java
+ *
+ * [515] 在每个树行中找最大值
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> largestValues(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        if (root == null) return ans;
+        LinkedList<TreeNode> queue = new LinkedList<>();
+        queue.addLast(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            int max = Integer.MIN_VALUE;
+            for (int i = 0; i < size; i++) {
+                TreeNode cur = queue.pollFirst();
+                max = Math.max(max, cur.val);
+                if (cur.left != null) queue.addLast(cur.left);
+                if (cur.right != null) queue.addLast(cur.right);
+            }
+            ans.add(max);
+        }
+        return ans;
+    }
+}
+// @lc code=end
+
