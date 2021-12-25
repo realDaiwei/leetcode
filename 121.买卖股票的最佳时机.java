f848c3566976b5f3bec2a79ee2f3c255977edcbb
DW <daiwei2333@gmail.com>
2021-12-25 13:30:04 +0800

    day47

---
+++ 121.买卖股票的最佳时机.java
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=121 lang=java
+ *
+ * [121] 买卖股票的最佳时机
+ */
+
+// @lc code=start
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxProfit = 0;
+        int minPrice = Integer.MAX_VALUE;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            } else if (maxProfit < prices[i] - minPrice) {
+                maxProfit = prices[i] - minPrice;
+            }
+        }
+        return maxProfit;
+    }
+}
+// @lc code=end
+
---
+++ 213.打家劫舍-ii.java
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=213 lang=java
+ *
+ * [213] 打家劫舍 II
+ */
+
+// @lc code=start
+class Solution {
+    public int rob(int[] nums) {
+        if (nums.length == 1) return nums[0];
+        if (nums.length == 2) return Math.max(nums[0], nums[1]);
+        return Math.max(baseRob(nums, 0, nums.length - 2), baseRob(nums, 1, nums.length - 1));
+    }
+
+    private int baseRob(int[] nums, int s, int e) {
+        if (e - s == 1) return Math.max(nums[s], nums[e]);
+        int[][] dp = new int[e - s + 1][2];
+        dp[0][1] = nums[s];
+        for (int i = 1; i < dp.length; i++) {
+            dp[i][1]  = dp[i - 1][0] + nums[s + i];
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1]);
+        }
+        return Math.max(dp[dp.length - 1][0], dp[dp.length - 1][1]);
+    }
+
+
+}
+// @lc code=end
+
