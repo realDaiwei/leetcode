d3c467f12c75b09f23aa9331219cd7ef8e50da14
DW <daiwei2333@gmail.com>
2021-12-29 23:22:35 +0800

    day52

---
+++ 279.完全平方数.java
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode.cn id=279 lang=java
+ *
+ * [279] 完全平方数
+ */
+
+// @lc code=start
+class Solution {
+    public int numSquares(int n) {
+        int dp[] = new int[n + 1];
+        for (int i = 0; i < n + 1; i++) {
+            dp[i] = i;
+            for (int j = 1; i - j * j >= 0; j++) {
+                dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+            }
+        }
+        return dp[n];
+    }
+}
+// @lc code=end
+
